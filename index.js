0205b7f6b8f771eeff68105c1fc42a5bcf0bbd59
Julia Speth <jsdev@mailbox.org>
2021-01-05 13:19:22 +0100

    Add index.js with preliminary classes

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-console.log('Hello world')
+class User {
+    constructor(name){
+      this.name = name
+      this.interests = []
+      this.organizes = []
+      this.bookmarked = []
+    }
+  
+    greet(person) {
+      console.log(`Hello ${person.name}, this is ${this.name}`)
+    } 
+  
+    addInterests(interest) {
+      this.interests.push(interest)
+    }
+  
+    organizeDemonstration(demo){
+      this.organizes.push(demo)
+      demo.organizedBy.push(this)
+    }
+  
+    bookmarkDemonstration(demo){
+        this.bookmarked.push(demo)
+        demo.bookmarkedBy.push(this)
+    }
+  }
+  
+  class Demonstration {
+   constructor(topic, location){
+    this.topic = topic
+    this.location = location
+    this.organizedBy = []
+    this.bookmarkedBy = []
+   }
+  }
+  
+  const julia = new User('Julia')
+  const caro = new User('Caro')
+  const regina = new User('Regina')
+  
+  const demo = new Demonstration('Climate change', 'Hamburg')
+  
+  julia.organizeDemonstration(demo)
+  caro.bookmarkDemonstration(demo)
+  regina.bookmarkDemonstration(demo)
+  
+  console.log(julia, julia.organizes[0].bookmarkedBy)
+  console.log(julia.greet(caro))
+  console.log(demo)
